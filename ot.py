2f2953c29029bf7d1df700c3fff8719aefd2305b
swimmingcircle119 <lampmaa22@gmail.com>
2024-03-19 17:08:56 -0700

    ot solutions

---
+++ ot.py
@@ -0,0 +1,6 @@
+class Operation: 
+    def __init__(self, position, value, operation):
+        self.position = position
+        self.value = value 
+        self.operation = operation #operation can only be 
+         
